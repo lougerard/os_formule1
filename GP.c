9a3559fd1edae39819e6fb35073fb8a911c03f06
root <root@localhost.localdomain>
2017-10-22 15:44:09 +0200

    ajustement

--- GP.c
+++ GP.c
@@ -3,11 +3,6 @@
 #include <stdlib.h>
 int main(int argc, char *argv[]) {
 
-        srand((unsigned int)time(NULL));
-        double a = 1.75;
-        int i=0;
-        for (i;i<20;i++)
-                printf("%f\n", ((double)rand()/(double)(RAND_MAX)) * a);
         return 0;
 
 
--- essai.c
+++ essai.c
@@ -1,6 +1,16 @@
 #include "rand.h"
 
-int main (int argc, char* argv[]){
-	printf("%f\n",1.2);
+double ourRandom(double truc);
 
+int main (int argc, char* argv[]){
+	printf("%f\n",ourRandom(1.2));
+
+}
+
+double ourRandom(double input) {
+
+        srand(time(NULL));
+        double randomNumber = (double) input * (double) rand() / (double) RAND_MAX;
+        //printf("%f\n", randomNumber);
+        return randomNumber;
 }
