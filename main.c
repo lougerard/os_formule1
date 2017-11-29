11dab072d893a7e9da2dc14167d8092b5a7b19a2
BJ2TL1 <root@localhost.localdomain>
2017-11-29 15:25:29 +0100

    testMain

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "circuit.c"
 
 int main (int argc, char* argv[]){
-	srand(time(NULL));
+ 	srand(time(NULL));
 	struct Circuit *circuit = malloc(sizeof(struct Circuit));
 	circuit->secteur1Min = 30.0;
 	circuit->secteur1Max = 40.0;
@@ -14,9 +14,27 @@ int main (int argc, char* argv[]){
 	circuit->secteur2Max = 35.0;
 	circuit->secteur3Min = 35.0;
 	circuit->secteur3Max = 40.0;
-	struct Voiture tabVoit[20];
-	struct Voiture *voit1 = malloc(sizeof(struct Voiture));
-	voitRoule(voit1, circuit);
-	//tabVoit[0] = voit1;
+	struct Voiture* tabVoit[20];
+	int j;
+	for( j = 0 ; j<2 ; j++){
+		struct Voiture *voit = malloc(sizeof (struct Voiture));
+		voit->numVoiture=j;
+		tabVoit[j] = voit;
+	}
+	pid_t pids[2];
+	int n = 2;
+	int i;
+	for( i = 0 ; i<n ; i++){
+		if((pids[i] = fork())<0){
+			perror("fork error");
+		}
+		/*else {
+			if(i!=0){
+				printf("Voiture numero %d \n", tabVoit[i]->numVoiture);
+				//voitRoule(tabVoit[i], circuit);
+			}
+		}*/
+		printf("Voiture numero %d \n", tabVoit[i]->numVoiture);
+	}
 	return 1;
 }
