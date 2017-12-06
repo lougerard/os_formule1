f80e4f8cfdae977f95d2f42511e2d3801f3d45ce
root <root@localhost.localdomain>
2017-12-06 15:47:48 +0100

    phil push

--- essai.h
+++ essai.h
@@ -6,55 +6,53 @@
 #include <stdlib.h>
 #include "rand.h"
 struct Voiture {
-	int numVoiture;
-	double tempsSecteur1;
-	double tempsSecteur2;
-	double tempsSecteur3; // temps actuel secteur 3
-	double tempsActuel;
+	int numVoiture;			//Numéro de la voiture
 
-	double meilleurTourEssai;
-	int nbrTourEssai1;
+	double tempsSecteur1;		//Temps actuel secteur 1
+	double tempsSecteur2;		//Temps actuel secteur 2
+	double tempsSecteur3; 		//Temps actuel secteur 3
+	double tempsActuel;		//Somme des secteurs en temps réel
 
-	double meilleurTourQualif;
-	int nbrTourQualif;
+	double meilleurTourEssai;	//Meilleur tour durant essais
+	int nbrTourEssai;		//Nbr. tour d'essais
 
-	double tempsFinal;
-	int positionFinal;
-	bool pitstop;
-	int nbrPitstop;
-	bool abandon;
+	double meilleurTourQualif;	//Meilleur tour durant qualifs
+	int nbrTourQualif;		////Nbr. tour de qualifs
+
+	bool pitstop;			//Si la voiture s'arrete au stand
+	bool abandon;			//Si la voiture a abandonner la course
 	}
 
 struct Circuit {
-	double secteur1;
-	double secteur2;
-	double secteur3;
-	double meilleurSecteur1;
-	int numVoitureS1;
-	double meilleurSecteur2;
-	int numVoitureS2;
-	double meilleurSecteur3;
-	int numVoitureS2;
-	}
-
-struct Essai {
-	double meilleurTourEssai;
-	}
+	double secteur1; 		//Temps réf. secteur 1
+	double secteur2;		//Temps réf. secteur 2
+	double secteur3;		//Temps réf. secteur 3
 	
-struct Qualification {
-	double meilleurTourQualif;
+	double meilleurSecteur1;	//Meilleur temps secteur 1
+	int numVoitureS1;		//Num. de la voiture la plus rapide S1
+	double meilleurSecteur2;	//Meilleur temps secteur 2
+	int numVoitureS2;		//Num. de la voiture la plus rapide S2
+	double meilleurSecteur3;	//Meilleur temps secteur 3
+	int numVoitureS2;		//Num. de la voiture la plus rapide S3
+	
+	double meilleurTourEssai;	//Meilleur tour durant les essais
+
+	double meilleurTourQualif;	//Meilleur tour durant les qualifs
+
+	double meilleurTourCourse;	//Meilleur tour durant la course
 	}
 
-struct Course {
-	int 
+struct Pitstop {
+	double temps pitstop; 		//Temps de pitstop référence
 	}
+
 struct Classement {
-	int position;
-	int numVoiture;
-	int nbrPitstop;
-	double tempsFinal
+	int position;			//Position final de la course
+	int numVoiture;			//Num. de la voiture par position
+	int nbrPitstop;			//Nombre de pitstop par voiture
+	int nbrTour;			//Nombre de tour pour la course
+	double tempsFinal;		//Temps total de la course
 	}
 
-Pitstop: temps a chaque pitstop
 #endif
 
