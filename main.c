5ea7b5e3ee6a32485160cc061fefb75f3b82b9b0
p-jongen <philemon.philj@gmail.com>
2018-01-15 11:44:23 +0100

    Fonction affichage tableau

--- main.c
+++ main.c
@@ -9,5 +9,5 @@ return 0;
 }
 
 void afficheLigne(int numVoiture, double tempsSecteur1, double tempsSecteur2, double tempsSecteur3, double tempsActuel, int nbrPitstop, int nbrTour){
-	printf("||%i     |%f      |%f     |%f   |%f      |%i     |%i     ||", numVoiture, tempsSecteur1, 		tempsSecteur2, tempsSecteur3, tempsActuel, nbrPitstop, nbrTour);
+	printf("||%i     |%f      |%f     |%f   |%f      |%i     |%i     ||", numVoiture, 		tempsSecteur1, tempsSecteur2, tempsSecteur3, tempsActuel, nbrPitstop, nbrTour);
 }
